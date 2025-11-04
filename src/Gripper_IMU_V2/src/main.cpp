745e0874555727706cbb77ea36353a5b7d04afd2
esole5 <esoletar69@alumnes.ub.edu>
2025-11-04 15:28:33 +0100

    correccions servo

--- src/Gripper_IMU_V2/src/main.cpp
+++ src/Gripper_IMU_V2/src/main.cpp
@@ -34,7 +34,7 @@ IMU imu;
 float Gri_roll = 0.0, Gri_pitch = 0.0, Gri_yaw = 0.0;
 
 // Torques received from servomotors module
-float Torque_roll1= 0.0; float Torque_pitch = 0.0; float Torque_yaw = 0.0;
+float Torque_roll1= 0.0; float Torque_roll2= 0.0;float Torque_pitch = 0.0; float Torque_yaw = 0.0;
 
 void connectToWiFi() {
   Serial.print("Connecting to Wi-Fi");
